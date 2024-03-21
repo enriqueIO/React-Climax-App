9dcdd5d1ee1e62524edf94e2f17b5898c15428ab
Enrique <100079208+enriqueIO@users.noreply.github.com>
2024-03-21 20:19:10 -0300

    Adding - Fist Commit
    First commit - Enviando o projeto inteiro para o repositório do git

---
+++ weather-app/src/App.js
@@ -0,0 +1,64 @@
+import "./App.css";
+import { useState } from "react";
+
+const api = {
+  key: "615467c361882f814497a25cda824ebc",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+
+  /*
+    Search button is pressed. Make a fetch call to the Open Weather Map API.
+  */
+  const searchPressed = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+        console.log(result);
+      });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* HEADER  */}
+        <h1 style={{color:'orange'}}>Climax</h1>
+
+        {/* Search Box - Input + Button  */}
+        <div>
+          <input
+            type="text"
+            placeholder="Pesquise uma cidade..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button onClick={searchPressed}>Search</button>
+        </div>
+
+        {/* If weather is not undefined display results from API */}
+        {typeof weather.main !== "undefined" ? (
+          <div>
+            {/* Location  */}
+            <p>{weather.name}</p>
+
+            <p>{weather.sys.country}</p>
+
+            {/* Temperature Celsius  */}
+            <p>{weather.main.temp}°C</p>
+
+            {/* Condition (Sunny ) */}
+            <p>{weather.weather[0].main}</p>
+            <p>({weather.weather[0].description})</p>
+          </div>
+        ) : (
+          ""
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
